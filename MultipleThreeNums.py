c9928d2113be07b5b1cafb2ab09b67fc184a4d48
WangShuo <202483890024@nuist.edu.cn>
2025-03-22 00:47:40 +0800

    add

---
+++ MultipleThreeNums.py
@@ -0,0 +1,11 @@
+def multiply_three_numbers(a, b, c):
+    return a * b * c
+
+if __name__ == "__main__":
+
+    num1 = 2
+    num2 = 3
+    num3 = 4
+    result = multiply_three_numbers(num1, num2, num3)
+    print("The product of {0}, {1}, and {2} is {3}".format(num1, num2, num3, result))
+
